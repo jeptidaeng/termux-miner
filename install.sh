ae7a527a2a0bc60f961986b721443844169d8191
jeptidaeng <52452968+jeptidaeng@users.noreply.github.com>
2025-04-06 21:22:24 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,14 +2,13 @@
 echo "Updating and installing required things"
 apt update && apt upgrade -y
 apt install wget
-apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ libtool
 echo "Cloning and starting compiling"
 git clone https://github.com/wong-fi-hung/termux-miner.git
 cd ~/termux-miner
-
+apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ libtool
 # Basic *nix build instructions:
 echo "./install.sh"
-if !  ./build-linux-arm.sh ..; then
+if ! ./build-linux-arm.sh ..; then
     echo "Error: build-linux-arm.sh configuration failed"
     exit 1
 else
@@ -21,5 +20,5 @@ if !    ; then
     echo "Error: Compiling failed"
     exit 1
 else
-     echo "Done... you can use run script now"
+   echo "Done... you can use run script now"
 fi
