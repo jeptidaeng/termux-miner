46b08888d83d370a25a7829ed0643b2ac0e9ed5c
jeptidaeng <52452968+jeptidaeng@users.noreply.github.com>
2025-04-07 22:18:26 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 echo "Updating and installing required things"
 apt update && apt upgrade -y
 apt install wget && apt install nano -y
+apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ libtool -y
 echo "Cloning and starting compiling"
 git clone https://github.com/wong-fi-hung/termux-miner.git
 cd ~/termux-miner
-apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ libtool -y
 # Basic *nix build instructions:
 echo "./install.sh"
-if ! ./autogen.sh ..; then
+if ! ./build-linux-arm.sh ..; then
     echo "Error: build-linux-arm.sh configuration failed"
     exit 1
 else
