6855fb77c7155b363259c08c1cedb37d4f22c80b
jeptidaeng <52452968+jeptidaeng@users.noreply.github.com>
2025-04-07 22:26:19 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ apt install wget && apt install nano -y
 apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ libtool -y
 echo "Cloning and starting compiling"
 git clone https://github.com/wong-fi-hung/termux-miner.git
-cd ~/termux-miner
+cd termux-miner
 # Basic *nix build instructions:
 echo "./install.sh"
 if ! ./build-linux-arm.sh ..; then
