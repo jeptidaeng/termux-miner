5307b4c184ff226ddaebb968ffcd5c87afe77ed7
Bangjay <52452968+jeptidaeng@users.noreply.github.com>
2025-04-10 00:02:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,7 @@ apt install wget -y
 apt install nano -y
 apt install git wget nano gh tmate build-essential make m4 libcurl4-openssl-dev libjansson-dev automake autoconf libtool autotools-dev cmake zlib1g-dev openssl libssl-dev -y
 echo "Cloning and starting compiling"
-git clone https://github.com/wong-fi-hung/termux-miner.git
-cd ~/termux-miner
+cd && git clone https://github.com/wong-fi-hung/termux-miner && cd termux-miner
 # Basic *nix build instructions:
 echo "make -j4"
 if ! ./build-linux-arm.sh ..; then
