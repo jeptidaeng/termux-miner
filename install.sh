0b119b14b4591420b416e0268a120943dac9e08c
Bangjay <52452968+jeptidaeng@users.noreply.github.com>
2025-04-26 13:30:40 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-# Linux build, optimised for ARM devices
-if [ ! -e configure ]; then
-echo "Creating configure..."
-apt-get install curl git wget nano -y
-apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ libtool -y
+
+set -e
+echo "Updating and installing required things"
+apt install wget cmake
 echo "Cloning and starting compiling"
 git clone https://github.com/wong-fi-hung/termux-miner.git
 cd ~/termux-miner
-# Basic *nix build instructions:
-echo "./install.sh"
-if ! ./build-linux-arm.sh ..; then
-    echo "Error: build-linux-arm.sh configuration failed"
+apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ libtool -y
+# cmake
+echo "Basic *nix build instructions"
+if ! build-linux-arm.sh ..; then
+    echo "Error: CMake configuration failed"
     exit 1
 else
-      ..
+    ./build-linux-arm.sh ..
 fi
 # compile
 echo "Compiling now"
-if !    ; then
+if ! make; then
     echo "Error: Compiling failed"
     exit 1
 else
-   echo "Done... you can use run script now"
+    make && echo "Done... you can use run script now"
 fi
